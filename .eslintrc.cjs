4795d8a1647e5a5aaa626c84ef6bacf89437382c
Tony Blard <tonyblard55@gmail.com>
2024-01-16 12:12:53 +0100

    Feat :  Add tailwind and postcss config to ignorePatterns

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,12 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:prettier/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: [
+    'dist',
+    '.eslintrc.cjs',
+    'tailwind.config.js',
+    'postcss.config.js',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
