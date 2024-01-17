6fd066e7bed49448111a4f4b38dacf6814f8af60
Tony Blard <tonyblard55@gmail.com>
2024-01-17 17:16:27 +0100

    Refactor : Add useGetPokemon in pokemon-list component

--- src/components/pokemon/pokemon-list.component.tsx
+++ src/components/pokemon/pokemon-list.component.tsx
@@ -1,46 +1,14 @@
 import Loading from '@components/loading/loading.component.tsx';
 import Error from '@components/error/error.component.tsx';
-import { PokemonListSchema, PokemonSchema } from '@schemas/pokemon.schema.ts';
-import { useQueries, useQuery } from 'react-query';
 import PokemonItem from './pokemon-item.component';
+import useGetPokemon from 'hooks/use-get-pokemon.hook';
 
 const PokemonsList = () => {
-  const getPokemon = () => {
-    return fetch('https://pokeapi.co/api/v2/pokemon')
-      .then((res) => res.json())
-      .then((data) => PokemonListSchema.parse(data));
-  };
-  const getPokemonByName = (name: string) => {
-    return fetch('https://pokeapi.co/api/v2/pokemon/' + name)
-      .then((res) => res.json())
-      .then((data) => PokemonSchema.parse(data));
-  };
+  const { results, isError, isLoading } = useGetPokemon();
 
-  const {
-    data: pokemons,
-    isError,
-    isLoading,
-  } = useQuery({
-    queryKey: ['pokemons'],
-    queryFn: getPokemon,
-    select: (result) => result.results.map((pokemon) => pokemon.name),
-  });
+  if (isLoading) return <Loading />;
 
-  const results = useQueries(
-    (pokemons || []).map((name) => {
-      return {
-        queryKey: ['pokemon', name],
-        queryFn: () => getPokemonByName(name),
-      };
-    }),
-  );
-
-  const isLoadingAll = results.some((result) => result.isLoading);
-  const isErrorAll = results.some((result) => result.isError);
-
-  if (isLoading || isLoadingAll) return <Loading />;
-
-  if (isError || isErrorAll) return <Error />;
+  if (isError) return <Error />;
 
   return (
     <>
