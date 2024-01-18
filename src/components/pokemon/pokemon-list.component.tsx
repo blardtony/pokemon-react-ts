bc370aea9467a25fe9eb66ee88da31bd8711f293
Tony Blard <tonyblard55@gmail.com>
2024-01-18 11:32:36 +0100

    Refactor : Add custom hook usePagination in pokemon list component

--- src/components/pokemon/pokemon-list.component.tsx
+++ src/components/pokemon/pokemon-list.component.tsx
@@ -2,32 +2,17 @@ import Loading from '@components/loading/loading.component.tsx';
 import Error from '@components/error/error.component.tsx';
 import PokemonItem from './pokemon-item.component.tsx';
 import useGetPokemon from 'hooks/use-get-pokemon.hook.ts';
-import { useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import usePagination from 'hooks/use-pagination.ts';
 
 const PokemonsList = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [page, setPage] = useState<number>(
-    Number(searchParams.get('page')) || 1,
-  );
+  const { page, limit, next, previous } = usePagination();
   const { results, isError, isLoading, isPreviousData, isNextData } =
-    useGetPokemon(page);
+    useGetPokemon(page, limit);
 
   if (isLoading) return <Loading />;
 
   if (isError) return <Error />;
 
-  const nextPage = () => {
-    if (!isNextData) return;
-    setPage(page + 1);
-    setSearchParams({ page: String(page + 1) });
-  };
-
-  const previousPage = () => {
-    if (!isPreviousData) return;
-    setPage(page - 1);
-    setSearchParams({ page: String(page - 1) });
-  };
   return (
     <>
       <h1 className="text-3xl font-bold">Pokemon App</h1>
@@ -40,14 +25,14 @@ const PokemonsList = () => {
       </div>
       <button
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
-        onClick={() => previousPage()}
+        onClick={() => previous(!!isPreviousData)}
         disabled={!isPreviousData}
       >
         Previous page
       </button>
       <button
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
-        onClick={() => nextPage()}
+        onClick={() => next(!!isNextData)}
         disabled={!isNextData}
       >
         Next page
